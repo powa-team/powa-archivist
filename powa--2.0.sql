4ff51c433f3ef8416fa5e9a0a6c17252a8e5de48
Julien Rouhaud <julien.rouhaud@free.fr>
2015-02-25 13:56:15 +0100

    Handle creation/suppression of supported extensions.

--- powa--2.0.sql
+++ powa--2.0.sql
@@ -94,7 +94,7 @@ CREATE TABLE powa_functions (
     operation text NOT NULL,
     function_name text NOT NULL,
     added_manually boolean NOT NULL default true,
-    CHECK (operation IN ('snapshot','aggregate','purge'))
+    CHECK (operation IN ('snapshot','aggregate','purge','unregister'))
 );
 
 INSERT INTO powa_functions (module, operation, function_name, added_manually) VALUES
@@ -244,6 +244,78 @@ SELECT pg_catalog.pg_extension_config_dump('powa_qualstats_quals_history','');
 SELECT pg_catalog.pg_extension_config_dump('powa_qualstats_quals_history_current','');
 SELECT pg_catalog.pg_extension_config_dump('powa_qualstats_constvalues_history','');
 SELECT pg_catalog.pg_extension_config_dump('powa_qualstats_constvalues_history_current','');
+
+CREATE OR REPLACE FUNCTION public.powa_check_created_extensions()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $_$
+DECLARE
+BEGIN
+    /* We have for now no way for a proper handling of this event,
+     * as we don't have a table with the list of supported extensions.
+     * So just call every powa_*_register() function we know each time an
+     * extension is created. Powa should be in a dedicated database and the
+     * register function handle to be called several time, so it's not critical
+     */
+    PERFORM public.powa_kcache_register();
+    PERFORM public.powa_qualstats_register();
+END;
+$_$;
+
+CREATE EVENT TRIGGER powa_check_created_extensions
+    ON ddl_command_end
+    WHEN tag IN ('CREATE EXTENSION')
+    EXECUTE PROCEDURE public.powa_check_created_extensions() ;
+
+CREATE OR REPLACE FUNCTION public.powa_check_dropped_extensions()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $_$
+DECLARE
+    funcname text;
+    v_state   text;
+    v_msg     text;
+    v_detail  text;
+    v_hint    text;
+    v_context text;
+BEGIN
+    WITH ext AS (
+        SELECT object_name
+        FROM pg_event_trigger_dropped_objects() d
+        WHERE d.object_type = 'extension'
+    )
+    SELECT function_name INTO funcname
+    FROM powa_functions f
+    JOIN ext ON f.module = ext.object_name
+    WHERE operation = 'unregister';
+
+    IF ( funcname IS NOT NULL ) THEN
+        BEGIN
+            RAISE DEBUG 'running %', funcname;
+            EXECUTE 'SELECT ' || quote_ident(funcname) || '()';
+        EXCEPTION
+          WHEN OTHERS THEN
+            GET STACKED DIAGNOSTICS
+                v_state   = RETURNED_SQLSTATE,
+                v_msg     = MESSAGE_TEXT,
+                v_detail  = PG_EXCEPTION_DETAIL,
+                v_hint    = PG_EXCEPTION_HINT,
+                v_context = PG_EXCEPTION_CONTEXT;
+            RAISE WARNING 'powa_check_dropped_extensions(): function "%" failed:
+                state  : %
+                message: %
+                detail : %
+                hint   : %
+                context: %', funcname, v_state, v_msg, v_detail, v_hint, v_context;
+        END;
+    END IF;
+END;
+$_$;
+
+CREATE EVENT TRIGGER powa_check_dropped_extensions
+    ON sql_drop
+    WHEN tag IN ('DROP EXTENSION')
+    EXECUTE PROCEDURE public.powa_check_dropped_extensions() ;
 
 CREATE OR REPLACE FUNCTION powa_take_snapshot() RETURNS void AS $PROC$
 DECLARE
@@ -473,9 +545,10 @@ BEGIN
         SELECT COUNT(*) > 0 INTO v_func_present FROM public.powa_functions WHERE module = 'pg_stat_kcache';
         IF ( NOT v_func_present) THEN
             INSERT INTO powa_functions (module, operation, function_name, added_manually)
-            VALUES ('pg_stat_kcache', 'snapshot', 'powa_kcache_snapshot', true),
-                   ('pg_stat_kcache', 'aggregate', 'powa_kcache_aggregate', true),
-                   ('pg_stat_kcache', 'purge', 'powa_kcache_purge', true);
+            VALUES ('pg_stat_kcache', 'snapshot',   'powa_kcache_snapshot',   false),
+                   ('pg_stat_kcache', 'aggregate',  'powa_kcache_aggregate',  false),
+                   ('pg_stat_kcache', 'unregister', 'powa_kcache_unregister', false),
+                   ('pg_stat_kcache', 'purge',      'powa_kcache_purge',      false);
         END IF;
     END IF;
 
@@ -594,9 +667,10 @@ BEGIN
         SELECT COUNT(*) > 0 INTO v_func_present FROM public.powa_functions WHERE function_name IN ('powa_qualstats_snapshot', 'powa_qualstats_aggregate', 'powa_qualstats_purge');
         IF ( NOT v_func_present) THEN
             INSERT INTO powa_functions (module, operation, function_name, added_manually)
-            VALUES ('pg_qualstats', 'snapshot', 'powa_qualstats_snapshot', false),
-                   ('pg_qualstats', 'aggregate', 'powa_qualstats_aggregate', false),
-                   ('pg_qualstats', 'purge', 'powa_qualstats_purge', false);
+            VALUES ('pg_qualstats', 'snapshot',   'powa_qualstats_snapshot',   false),
+                   ('pg_qualstats', 'aggregate',  'powa_qualstats_aggregate',  false),
+                   ('pg_qualstats', 'unregister', 'powa_qualstats_unregister', false),
+                   ('pg_qualstats', 'purge',      'powa_qualstats_purge',      false);
         END IF;
     END IF;
 
@@ -737,7 +811,7 @@ $PROC$ language plpgsql;
 CREATE OR REPLACE function public.powa_qualstats_unregister() RETURNS bool AS
 $_$
 BEGIN
-    DELETE FROM public.powa_functions WHERE function_name IN ('powa_qualstats_snapshot', 'powa_qualstats_aggregate', 'powa_qualstats_purge');
+    DELETE FROM public.powa_functions WHERE module = 'pg_qualstats';
     RETURN true;
 END;
 $_$
