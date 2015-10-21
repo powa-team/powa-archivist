2a03acf95b3faa69dc9957f3e36389402cda75aa
Julien Rouhaud <julien.rouhaud@free.fr>
2015-10-21 14:49:11 +0200

    Don't change check_function_bodies during CREATE EXTENSION

--- powa--3.0.0.sql
+++ powa--3.0.0.sql
@@ -4,7 +4,6 @@
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 SET search_path = public, pg_catalog;
