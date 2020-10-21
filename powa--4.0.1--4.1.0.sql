7fc8cd26bd1a42fb18e30bf8695384e813cfd9ac
Julien Rouhaud <julien.rouhaud@free.fr>
2020-10-21 11:37:22 +0800

    2 args current_setting was added in pg9.6, not 9.5

--- powa--4.0.1--4.1.0.sql
+++ powa--4.0.1--4.1.0.sql
@@ -1,3 +1,41 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 --\echo Use "ALTER EXTENSION powa" to load this file. \quit
 
+DROP FUNCTION powa_log(text);
+DO $anon$
+BEGIN
+    IF current_setting('server_version_num')::int < 90600 THEN
+        CREATE FUNCTION powa_log (msg text) RETURNS void
+        LANGUAGE plpgsql
+        AS $_$
+        DECLARE
+            v_debug bool;
+        BEGIN
+            BEGIN
+                SELECT current_setting('powa.debug')::bool INTO v_debug;
+            EXCEPTION WHEN OTHERS THEN
+                v_debug = false;
+            END;
+            IF v_debug THEN
+                RAISE WARNING '%', msg;
+            ELSE
+                RAISE DEBUG '%', msg;
+            END IF;
+        END;
+        $_$;
+    ELSE
+        CREATE FUNCTION powa_log (msg text) RETURNS void
+        LANGUAGE plpgsql
+        AS $_$
+        BEGIN
+            IF COALESCE(current_setting('powa.debug', true), 'off')::bool THEN
+                RAISE WARNING '%', msg;
+            ELSE
+                RAISE DEBUG '%', msg;
+            END IF;
+        END;
+        $_$;
+    END IF;
+END;
+$anon$;
+
