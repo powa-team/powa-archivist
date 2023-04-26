4fccf4e928158717c18194dc4464feb8c208297d
Julien Rouhaud <julien.rouhaud@free.fr>
2023-04-26 17:04:27 +0800

    Start working on 4.2.0
    Recent bugs dicovered by Marc Cousin will require changes in both
    powa-archivist and powa-web to store the pgss toplevel field, so we need to
    bump the major version.
    
    While at it remove the long oudated 3.2.0 install script.

---
+++ powa--4.1.4--4.2.0.sql
@@ -0,0 +1,9 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+--\echo Use "ALTER EXTENSION powa" to load this file. \quit
+SET LOCAL statement_timeout = 0;
+SET LOCAL client_encoding = 'UTF8';
+SET LOCAL standard_conforming_strings = on;
+SET LOCAL client_min_messages = warning;
+SET LOCAL escape_string_warning = off;
+SET LOCAL search_path = public, pg_catalog;
+
