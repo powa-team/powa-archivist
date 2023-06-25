18584c371704db446843218838027a5652da12b0
Julien Rouhaud <julien.rouhaud@free.fr>
2023-06-25 21:31:01 +0800

    Add powa_* pseudo predefined roles
    6 new pseudo predefined roles are added:
    
    - powa_admin:            has all privileges on all relations
    - powa_read_all_data:    has SELECT only privilege on all relation useful to
                             consume the data from a client point of view.
    - powa_read_all_metrics: same as powa_read_all_data, but without access to
                             possibly sensitive data
    - powa_write_all_data:   has minimum read and write privileges on all relations
                             useful for all regular operations (e.g. performing a
                             snapshot) and other operations (e.g. removing all data
                             from a remote server)
    - powa_snapshot:         has minimum read and write privileges on all relations
                             useful for performing a snapshot
    - powa_signal_backend:   doesn't have any privilege on any relation, but can be
                             checked by powa-collector and powa-web to allow the
                             use of the notification system (force a collector
                             reload, force a snapshot and so on)
    
    Unlike real predefined roles, we can't blindly create them or assume we could
    do so.
    
    powa_read_all_data and powa_read_all_metrics are meant as a privilege to grant
    for the roles meant to access powa-web, or consume the data in a similar way.
    The difference between the two is that powa_read_all_data can also access
    possiblty sensitive data, ie. the const values gathered by pg_qualstats.
    
    powa_write_all_data is meant as a privilege to grant for having complete access
    to all features, creating / removing / updating remote servers and so on.
    
    powa_snapshot is meant as a privilege to grant for the role defined in
    powa-collector connection to perform the snapshots.
    
    As roles are global objects they're not removed with the rest of an extension
    dependencies.  It means that simply creating and dropping powa will leave the
    pseudo predefined roles, but we can't assume that any existing powa_* role has
    been created by us and can be reused for our own purpose.
    
    To make it work this patch adds a new powa_roles table which contains a mapping
    between our logical roles (e.g. powa_admin) and the effective role on the
    instance if any.  By default the effective role is NULL which means that we
    assume that no such role exists and act that way.
    
    A new setup_powa_roles() is provided as a wrapper to create all our pseudo
    predefined roles.  It contains an argument per pseudo role to be able to
    override the wanted role name, plus a boolean argument to decide to reuse
    existing role.
    
    Reusing existing roles isn't done by default for security reason, and users
    have to explicitly ask for it.  Even when doing so, some sanity checks are done
    to make sure that the existing roles don't have any unwanted attribute (they
    shouldn't be superuser, shouldn't be able to log in and so on).
    
    Two extra functions, powa_grant() and powa_revoke() are also provided, to
    properly setup or remove the ACL for the various pseudo predefined roles.  Note
    that setup_powa_roles() will automatically call powa_grant().
    
    All those functions are defined as SECURITY INVOKER, so the caller needs to
    have enough privileges for all the underlying actions.
    
    In order to help the default use case (powa installed only once in a single
    database), the create extension script will check if none of the default pseudo
    predefined role exist, and in that case will create them and grant all the
    required privileges.  Any other scenario (dropping and recreating the
    extension, restoring from a logical backup, installing powa in multiple
    databases...) require explicit actions from the administrator if they want to
    benefit from those pseudo predefined roles.

--- sql/99_cleanup.sql
+++ sql/99_cleanup.sql
@@ -1,3 +1,6 @@
+-- General setup
+\set SHOW_CONTEXT never
+
 -- Check for local server reset
 SELECT "PoWA".powa_reset(0);
 
@@ -70,3 +73,33 @@ $_$ LANGUAGE plpgsql;
 
 -- Check remote server removal
 DELETE FROM "PoWA".powa_servers WHERE id = 1;
+
+-- dropping powa will leave the role initially created, and we should be able
+-- to reinstall powa again
+DROP EXTENSION powa;
+CREATE EXTENSION powa WITH SCHEMA "PoWA";
+
+-- we shouldn't assigned the existing role to our pseudo predefined role, even
+-- if they exist with the default name
+SELECT * FROM "PoWA".powa_roles WHERE rolname IS NOT NULL;
+
+-- and also shouldn't put back any ACL on the previous roles
+SELECT has_table_privilege('powa_admin', '"PoWA".powa_servers', 'SELECT');
+
+-- We shouldn't be able to grant or revoke privileges in that situation
+SELECT "PoWA".powa_revoke();
+SELECT "PoWA".powa_grant();
+
+-- we should be able to reuse the initially created roles
+SELECT "PoWA".setup_powa_roles(true);
+SELECT has_table_privilege('powa_admin', '"PoWA".powa_servers', 'SELECT');
+
+-- cleanup ACL and remove the powa pseudo predefined roles
+SELECT "PoWA".powa_revoke();
+SELECT has_table_privilege('powa_admin', '"PoWA".powa_servers', 'SELECT');
+DROP ROLE powa_admin;
+DROP ROLE powa_read_all_data;
+DROP ROLE powa_read_all_metrics;
+DROP ROLE powa_write_all_data;
+DROP ROLE powa_snapshot;
+DROP ROLE powa_signal_backend;
