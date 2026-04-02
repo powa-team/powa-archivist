76306839c694d397b64a1ed474639a0bd43eacd6
Georgy Shelkovy <rekgrpth@gmail.com>
2026-04-02 08:11:34 +0500

    Fix Postgres 19 support
    Commit postgres/postgres@76f4b92 changed the order of the PGDLLEXPORT
    pg_noreturn attributes to pg_noreturn PGDLLEXPORT.

--- powa.c
+++ powa.c
@@ -84,7 +84,7 @@ PG_FUNCTION_INFO_V1(powa_stat_user_functions);
 PG_FUNCTION_INFO_V1(powa_stat_all_rel);
 
 #if (PG_VERSION_NUM >= 180000)
-PGDLLEXPORT pg_noreturn void powa_main(Datum main_arg);
+pg_noreturn PGDLLEXPORT void powa_main(Datum main_arg);
 #elif (PG_VERSION_NUM >= 90500)
 PGDLLEXPORT void powa_main(Datum main_arg) pg_attribute_noreturn();
 #else
